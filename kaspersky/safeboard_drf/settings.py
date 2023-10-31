fc6d5a760883273d679e4f92eb1b811457e1900e
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-10-31 21:59:04 +0300

    Change settings and url in DRF

--- kaspersky/safeboard_drf/settings.py
+++ kaspersky/safeboard_drf/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "safeboard_drf.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.joinpath('safeboard_react')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- kaspersky/safeboard_drf/urls.py
+++ kaspersky/safeboard_drf/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from employees.views import EmployeesAPIList
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, 'build/index.html')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/employees/", EmployeesAPIList.as_view()),
+    path('', index_view, name='index'),
 ]
